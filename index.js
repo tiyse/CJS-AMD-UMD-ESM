d0e3252680b174266bbaf7562b4bd24328807c10
tubram <95336536+tubram@users.noreply.github.com>
2023-11-13 11:02:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,95 @@
+// CJS - CommonJS
+// ---------------------------------------------------------------
+//importing 
+const doSomething = require('./doSomething.js'); 
+
+//exporting
+module.exports = function doSomething(n) {
+  // do something
+}
+
+// AMD - Asynchronous Module Definition
+// ---------------------------------------------------------------
+define(['dep1', 'dep2'], function (dep1, dep2) {
+    //Define the module value by returning a value.
+    return function () {};
+});
+
+// or
+
+// "simplified CommonJS wrapping" https://requirejs.org/docs/whyamd.html
+define(function (require) {
+    var dep1 = require('dep1'),
+        dep2 = require('dep2');
+    return function () {};
+});
+
+// UMD - Universal Module Definition
+// ---------------------------------------------------------------
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(["jquery", "underscore"], factory);
+    } else if (typeof exports === "object") {
+        module.exports = factory(require("jquery"), require("underscore"));
+    } else {
+        root.Requester = factory(root.$, root._);
+    }
+}(this, function ($, _) {
+    // this is where I defined my module implementation
+
+    var Requester = { // ... };
+
+    return Requester;
+}));
+
+// IIFE - Immediately Invoked Function Expression
+// ---------------------------------------------------------------
+import React from 'react';
+
+import {foo, bar} from './myLib';
+
+...
+
+export default function() {
+  // your Function
+};
+export const function1() {...};
+export const function2() {...};
+
+// ESM - ECMAScript Module
+// ---------------------------------------------------------------
+var myPersonalInfoModule = function () {
+
+        // those variables are private and are not accessible from outside
+        let name = "Mary";
+        let surname  = "Mcfly";
+
+        // those methods are private and are not accessible from outside
+        function concatenateName() {
+            return `${name} ${surname}`
+        }
+
+        function printName(){
+          console.log(concatenateName())
+        }
+        
+        function setName( newName ) {
+            name = newName;
+        }
+
+        function setSurname( newSurname ) {
+            surname = newSurname;
+        }
+
+        // this way we expose those variables and methods we want
+        return {
+            setName: setName,
+            setSurname: setSurname,
+            printFullName: printName
+        };
+
+    }();
+
+// example of use
+myPersonalInfoModule.setName( "Doc" );
+myPersonalInfoModule.setSurname( "Brown" );
